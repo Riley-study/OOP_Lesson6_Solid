8cfffbdc0586176d33a40bb7b822792c52e0cbb3
Riley-study <120293475+Riley-study@users.noreply.github.com>
2023-06-14 10:49:24 +0500

    task 7 solved

--- Solid7Dip/src/solid/dip/Report.java
+++ Solid7Dip/src/solid/dip/Report.java
@@ -3,18 +3,22 @@ package solid.dip;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Report{
-    private List<ReportItem> items;	// Отчетные данные
+public class Report {
+    private List<ReportItem> items;    // Отчетные данные
+    private Reportable reportable;
 
-    // расчет отчетных данных
-    public void calculate(){
-        items =  new ArrayList<ReportItem>();
-        items.add(new ReportItem("First", (float)5));
-        items.add(new ReportItem("Second", (float)6));
+    public Report(Reportable reportable) {
+        this.reportable = reportable;
     }
 
-    public void output(){
-        PrintReport reportPrint = new PrintReport();
-        reportPrint.output(items);
+    // расчет отчетных данных
+    public void calculate() {
+        items = new ArrayList<ReportItem>();
+        items.add(new ReportItem("First", (float) 5));
+        items.add(new ReportItem("Second", (float) 6));
+    }
+
+    public void output() {
+        reportable.output(items);
     }
 }
